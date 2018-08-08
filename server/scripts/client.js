1e1aecd80ce87d382f57435d92edf421946c4aae
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2018-08-08 00:36:21 -0500

    Time changed on DOM reflects in the printout version

--- server/scripts/client.js
+++ server/scripts/client.js
@@ -1,24 +1,10 @@
 $(document).ready(function() {
   console.log('Ready');
 
-$( "#printButton" ).click(function() {
-  window.print();
-});
-
-$( "#time-start" ).change(function() {
-  //get the time value
-  //update the printout
-});
-
-$( "#time-end" ).change(function() {
-  //get the time value
-  //update the printout
-});
-
 //Temporary object to be replaced by data from backend.
 var arrayOfObj = [];
-var obj1 = new Obj("https://1.api.artsmia.org/800/7505.jpg","Japanese and Korean Art", "134", "The collection of Japanese and Korean art includes more than 7,000 works ranging from ancient to contemporary and is among the top five collections in the United States. The permanent display space for Japanese art is the largest in the Western world with 15 galleries and over 10,000 square feet (930 sqm). The collection itself includes Buddhist sculpture, woodblock prints, paintings, lacquer, works of bamboo, and ceramics, and it is particularly rich in works from the Edo period (1610–1868).", "9:00am", "9:15am");
-var obj2 = new Obj("https://3.api.artsmia.org/800/1704.jpg", "Contemporary Art", "342", "In 2008 Mia launched an initiative to focus on the art of our times. Through its new Department of Contemporary Art, the museum brings a fresh dynamism to its galleries by collecting and exhibiting works by living artists. This initiative emphasizes the relationships among historical art, diverse cultures, and contemporary art-making.", "09:00:00", "09:15:00");
+var obj1 = new Obj("https://1.api.artsmia.org/800/7505.jpg","Japanese and Korean Art", "134", "The collection of Japanese and Korean art includes more than 7,000 works ranging from ancient to contemporary and is among the top five collections in the United States. The permanent display space for Japanese art is the largest in the Western world with 15 galleries and over 10,000 square feet (930 sqm). The collection itself includes Buddhist sculpture, woodblock prints, paintings, lacquer, works of bamboo, and ceramics, and it is particularly rich in works from the Edo period (1610–1868).", "09:00:00", "09:15:00");
+var obj2 = new Obj("https://3.api.artsmia.org/800/1704.jpg", "Contemporary Art", "342", "In 2008 Mia launched an initiative to focus on the art of our times. Through its new Department of Contemporary Art, the museum brings a fresh dynamism to its galleries by collecting and exhibiting works by living artists. This initiative emphasizes the relationships among historical art, diverse cultures, and contemporary art-making.", "09:15:00", "09:30:00");
 function Obj(img, title, galleryNum, description, timeStart, timeEnd) {
     this.imgUrl = img;
     this.title = title;
@@ -32,6 +18,23 @@ arrayOfObj.push(obj1);
 arrayOfObj.push(obj2);
 
 populateDOM(arrayOfObj);
+
+  $( "#printButton" ).click(function() {
+    window.print();
+  });
+  $('body').on('blur', '.time-start', function(e) {
+    var inputVal = e.target.value;
+    var timeClass = '#hidden-print-div .' + $(event.target).attr('class').split(" ")[1];
+    console.log($(timeClass).text( ));
+    $(timeClass).text( inputVal );
+  });
+
+  $('body').on('blur', '.time-end', function(e) {
+    var inputVal = e.target.value;
+    var timeClass = '#hidden-print-div .' + $(event.target).attr('class').split(" ")[1];
+    console.log($(timeClass).text( ));
+    $(timeClass).text( inputVal );
+  });
 
 function populateDOM(arrayOfObj){
   for(var i = 0; i < arrayOfObj.length ; i++){
@@ -51,10 +54,10 @@ function populateDOM(arrayOfObj){
       '<div class="col-3 grid-layout" style="background-color: white;">' +
           '<table>' +
             '<tr>' +
-              '<td>Start Time:</td><td> <input id="time-start" class="time-start-'+ [i] + '" type="time" placeholder="' + arrayOfObj[i].startTime + '" aria-label=""></td>' +
+              '<td>Start Time:</td><td> <input class="time-start time-start-'+ [i] + '" type="time" value="' + arrayOfObj[i].timeStart + '" aria-label=""></td>' +
             '</tr>' +
             '<tr>' +
-            '<td>End Time:</td>  <td><input id="time-end" class="time-end-'+ [i] + '" type="time" value="' + arrayOfObj[i].endTime + '" aria-label=""></td>' +
+            '<td>End Time:</td>  <td><input class="time-end time-end-'+ [i] + '" type="time" value="' + arrayOfObj[i].timeEnd + '" aria-label=""></td>' +
           '</tr>' +
           '</table>' +
       '</div>';
@@ -73,10 +76,10 @@ function populateDOM(arrayOfObj){
             '<h3>Room #'+ arrayOfObj[i].galleryNum + '</h3>' +
             '<table>' +
               '<tr>' +
-                '<td style="font-weight: bold;">Start Time:</td><td class="class="time-start-'+ [i] + '">' + arrayOfObj[i].startTime + '</td>' +
+                '<td style="font-weight: bold;">Start Time:</td><td class="time-start-'+ [i] + '"> ' + arrayOfObj[i].timeStart + '</td>' +
               '</tr>' +
               '<tr>' +
-              '<td style="font-weight: bold;">End Time:</td><td class="class="time-end-'+ [i] + '"> ' + arrayOfObj[i].endTime + '</td>' +
+              '<td style="font-weight: bold;">End Time:</td><td class="time-end-'+ [i] + '"> ' + arrayOfObj[i].timeEnd + '</td>' +
             '</tr>' +
             '</table>' +
         '</tr>' +
