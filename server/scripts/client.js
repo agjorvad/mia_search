d52334ca6cf5af20d48bf3a4b543621d433ee5ce
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2018-08-08 19:43:31 -0500

    Adding move up and down functionality to rows

--- server/scripts/client.js
+++ server/scripts/client.js
@@ -3,8 +3,11 @@ $(document).ready(function() {
 
 //Temporary object to be replaced by data from backend.
 var arrayOfObj = [];
-var obj1 = new Obj("https://1.api.artsmia.org/800/7505.jpg","Japanese and Korean Art", "134", "The collection of Japanese and Korean art includes more than 7,000 works ranging from ancient to contemporary and is among the top five collections in the United States. The permanent display space for Japanese art is the largest in the Western world with 15 galleries and over 10,000 square feet (930 sqm). The collection itself includes Buddhist sculpture, woodblock prints, paintings, lacquer, works of bamboo, and ceramics, and it is particularly rich in works from the Edo period (1610–1868).", "09:00:00", "09:15:00");
+var obj1 = new Obj("https://1.api.artsmia.org/800/7505.jpg","Japanese and Korean Art", "234", "The collection of Japanese and Korean art includes more than 7,000 works ranging from ancient to contemporary and is among the top five collections in the United States. The permanent display space for Japanese art is the largest in the Western world with 15 galleries and over 10,000 square feet (930 sqm). The collection itself includes Buddhist sculpture, woodblock prints, paintings, lacquer, works of bamboo, and ceramics, and it is particularly rich in works from the Edo period (1610–1868).", "09:00:00", "09:15:00");
 var obj2 = new Obj("https://3.api.artsmia.org/800/1704.jpg", "Contemporary Art", "342", "In 2008 Mia launched an initiative to focus on the art of our times. Through its new Department of Contemporary Art, the museum brings a fresh dynamism to its galleries by collecting and exhibiting works by living artists. This initiative emphasizes the relationships among historical art, diverse cultures, and contemporary art-making.", "09:15:00", "09:30:00");
+var obj3 = new Obj("https://4.api.artsmia.org/800/89324.jpg", "Photography and New Media", "270", "Begun in 1973, Mia’s collection of photographs spans the history of photography from the 1860s to the present. Representing more than 800 photographers and 11,500 works of art, the collection has outstanding examples of twentieth-century American photography, with particular depth in the genres of documentary photography, photojournalism, and pictorialism. Since 2008, the museum has focused on expanding its holdings of contemporary photography and new media from all countries. This initiative increases the scope and relevance of the photography collection and enhances its links to Mia’s diverse, global holdings.", "09:30:00", "09:45:00");
+
+
 function Obj(img, title, galleryNum, description, timeStart, timeEnd) {
     this.imgUrl = img;
     this.title = title;
@@ -16,6 +19,8 @@ function Obj(img, title, galleryNum, description, timeStart, timeEnd) {
 
 arrayOfObj.push(obj1);
 arrayOfObj.push(obj2);
+arrayOfObj.push(obj3);
+
 
 populateDOM(arrayOfObj);
 
@@ -36,10 +41,30 @@ populateDOM(arrayOfObj);
     $(timeClass).text( inputVal );
   });
 
+  $('body').on('click', '.btn-up', function(e){
+    //MOVE ROW UP IN DOM AND PRINTER VIEW
+    var target = e.currentTarget.parentElement.parentElement.parentElement.id;
+    var domRow = $("#" + target);
+    var printRow = $("#print-" + target);
+    domRow.prev().insertAfter(domRow);
+    printRow.prev().insertAfter(printRow);
+    //TRADE TIME VALUES
+  });
+
+  $('body').on('click', '.btn-down', function(e){
+    //MOVE ROW DOWN IN DOM AND PRINTER VIEW
+    var target = e.currentTarget.parentElement.parentElement.parentElement.id;
+    var domRow = $("#" + target);
+    var printRow = $("#print-" + target);
+    domRow.next().insertBefore(domRow);
+    printRow.next().insertBefore(printRow);
+    //TRADE TIME VALUES
+  });
+
 function populateDOM(arrayOfObj){
   for(var i = 0; i < arrayOfObj.length ; i++){
-    var appendDiv =
-      '<div class="col-3 grid-layout">' +
+    var appendDiv = '<div id="row-'+ [i] + '">' +
+      '<div id="row-'+ [i] + '" class="col-3 grid-layout">' +
         '<img style="max-height: 500px;" src="' + arrayOfObj[i].imgUrl + '"/>' +
       '</div>' +
       '<div class="col-3 grid-layout" style="padding: 10px;">' +
@@ -52,7 +77,7 @@ function populateDOM(arrayOfObj){
         '</p>' +
       '</div>' +
       '<div class="col-3 grid-layout" style="background-color: white;">' +
-          '<table>' +
+          '<table style="float: left;">' +
             '<tr>' +
               '<td>Start Time:</td><td> <input class="time-start time-start-'+ [i] + '" type="time" value="' + arrayOfObj[i].timeStart + '" aria-label=""></td>' +
             '</tr>' +
@@ -60,12 +85,16 @@ function populateDOM(arrayOfObj){
             '<td>End Time:</td>  <td><input class="time-end time-end-'+ [i] + '" type="time" value="' + arrayOfObj[i].timeEnd + '" aria-label=""></td>' +
           '</tr>' +
           '</table>' +
-      '</div>';
+          '<div style="float:right; top: 0px;">' +
+          '<button style="width: 60px;" class="btn-up background-black">Up</button></br>' +
+          '<button style="width: 60px;" class="btn-down background-black">Down</button>'+
+          '</div>'
+      '</div></div>';
 
       $("#schedulePrintout").append(appendDiv);
 
       var appendDivPrintable =
-      '<table>' +
+      '<div id="print-row-'+ [i] +'"><table>' +
         '<tr>' +
           '<td style="width: 500px;">' +
             '<img style="float: left; padding: 0 15px; max-height: 140px;" src="' + arrayOfObj[i].imgUrl + '"/>' +
@@ -76,14 +105,16 @@ function populateDOM(arrayOfObj){
             '<h3>Room #'+ arrayOfObj[i].galleryNum + '</h3>' +
             '<table>' +
               '<tr>' +
-                '<td style="font-weight: bold;">Start Time:</td><td class="time-start-'+ [i] + '"> ' + arrayOfObj[i].timeStart + '</td>' +
+                '<td style="font-weight: bold;">Start Time:</td>' +
+                '<td class="time-start-'+ [i] + '"> ' + arrayOfObj[i].timeStart + '</td>' +
               '</tr>' +
               '<tr>' +
-              '<td style="font-weight: bold;">End Time:</td><td class="time-end-'+ [i] + '"> ' + arrayOfObj[i].timeEnd + '</td>' +
+              '<td style="font-weight: bold;">End Time:</td>' +
+              '<td class="time-end-'+ [i] + '"> ' + arrayOfObj[i].timeEnd + '</td>' +
             '</tr>' +
             '</table>' +
         '</tr>' +
-      '</table>';
+      '</table></div>';
 
       $("#hidden-print-div").append(appendDivPrintable);
   }
